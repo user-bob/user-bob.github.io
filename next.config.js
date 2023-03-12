7e331c7762826e719629720153f440eb938d98c1
Bernard Yaw <bernardyaw02@gmail.com>
2023-03-12 15:50:50 -0700

    added repo name to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   output: 'export',
+  basePath: !debug ? '/user-bob.github.io' : '',
+  assetPrefix: !debug ? '/user-bob.github.io' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 module.exports = nextConfig
