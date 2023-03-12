6ee9965769b90875e9d67066e5c588bfdf251e17
user-bob <111507754+user-bob@users.noreply.github.com>
2023-03-12 14:09:58 -0700

    Nextjs 13 beta base skeleton

---
+++ src/lib/firebase/init.ts
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const createFirebaseApp = () =>  {
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  };
+
+  // Initialize Firebase
+  if (getApps().length <= 0) {
+    const app = initializeApp(firebaseConfig)
+    // Check that `window` is in scope for the analytics module!
+    if (typeof window !== 'undefined') {
+      // Enable analytics. https://firebase.google.com/docs/analytics/get-started
+      if ('measurementId' in firebaseConfig) {
+        getAnalytics()
+      }
+    }
+    return app
+  }
+
+}
