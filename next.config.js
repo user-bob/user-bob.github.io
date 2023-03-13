0900e4729cfeb6bfa7a709252f6134772ec0e1d0
user-bob <111507754+user-bob@users.noreply.github.com>
2023-03-13 04:11:45 -0700

    done

--- next.config.js
+++ next.config.js
@@ -4,16 +4,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  output: 'export',
+  // output: 'export',
   basePath: '/user-bob.github.io',
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-    }
-  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //   }
+  // },
 }
 
 module.exports = nextConfig
