86deee569a0d538b2f0bbc713e8221267e5b63b4
user-bob <111507754+user-bob@users.noreply.github.com>
2023-09-17 16:44:39 +0100

    Update firestore.ts

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -3,8 +3,11 @@ import {initFirestore} from "@auth/firebase-adapter";
 
 export const firestore = initFirestore({
     credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        projectId: "*****",
+        clientEmail: "******",
+        privateKey: "******",
+        // projectId: process.env.FIREBASE_PROJECT_ID,
+        // clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // privateKey: process.env.FIREBASE_PRIVATE_KEY,
     })
 })
