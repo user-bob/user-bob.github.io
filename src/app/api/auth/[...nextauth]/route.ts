c77c414e2a9f4c306d20d0a0aef62f3c4f1fb01b
Bobie Brighton <bobiebrighton@gmail.com>
2023-09-18 08:54:37 +0100

    Uncomment authentication implementation and update environments
    Enabled authentication by uncommenting previous implementation that was temporarily disabled for testing purposes. Updated firebase Firestore credentials in the src/lib/firestore.ts file to use environment variables, improving security. The src/app/api/auth/[...nextauth]/route.ts file was modified to use authOptions, which is now active after uncommenting in auth.ts.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,6 @@
 import NextAuth from "next-auth"
-// import {authOptions} from "@/lib/auth";
+import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth({
-	// Configure one or more authentication providers
-	providers: []
-})
-// const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
